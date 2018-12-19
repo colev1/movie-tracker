0683281fa0cf1bb68bdc8e93b48ba6d10eabf4de
Kylie Stanley <knstanley11@gmail.com>
2018-12-19 15:26:38 -0700

    Add function to log user out

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,3 +4,9 @@ export const loginUser = (username) => {
     username
   }
 }
+
+export const logout = () => {
+  return {
+    type: 'LOGOUT_USER'
+  }
+}
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -4,6 +4,8 @@ export const userReducer = (state = '', action) => {
   switch(action.type) {
     case 'LOGIN_USER':
       return action.username
+    case 'LOGOUT_USER': 
+      return ''
     default:
       return state
   }
