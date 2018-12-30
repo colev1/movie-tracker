4559fdccc7123cc3ff20a06a08937d1691282b37
Kylie Stanley <knstanley11@gmail.com>
2018-12-30 11:30:11 -0700

    Tests for matching favorites

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Card, mapStateToProps, mapDispatchToProps } from './Card'
 import { connect } from 'react-redux'
+import * as API from '../API';
 import * as actions from '../actions'
+
 
 describe('Card', () => {
   let wrapper
@@ -11,21 +13,50 @@ describe('Card', () => {
   let mockUser
 
   beforeEach(() => {
-    mockMovie = {title: 'movie1'}
-    mockFavorites = [{title: 'movie1'}, {title: 'movie2'}]
+    mockMovie = {title: 'movie1', movie_id: 1}
+    mockFavorites = [{title: 'movie1', movie_id: 1}, {title: 'movie2', movie_id: 2}]
     mockUser = { name: 'Taylor', id: 1 }
     wrapper = shallow(
       <Card
         movie={ mockMovie }
-        key={'movie1'}
+        key={ mockMovie.title }
         favorites= { mockFavorites }
         user={ mockUser }
+        addFavorite = { jest.fn() }
+        deleteFavoriteFromStore = { jest.fn() }
       />
     )
   })
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should call postFavorite if there is no match', () => {
+    let mockMovie = {title: 'movie1', movie_id: 3}
+    let wrapper = shallow(
+      <Card
+        movie={ mockMovie }
+        key={ mockMovie.title }
+        favorites= { mockFavorites }
+        user={ mockUser }
+        addFavorite = { jest.fn() }
+        deleteFavoriteFromStore = { jest.fn() }
+      />
+    )
+    API.postFavorite = jest.fn()
+    const button = wrapper.find('button');
+    const handleSpy = jest.spyOn(wrapper.instance(), 'toggleFavorite')
+    button.simulate('click');
+    expect(API.postFavorite).toHaveBeenCalled()
+  })
+
+  it('should call delete if there is a match', () => {
+    API.deleteFavorite = jest.fn()
+    const button = wrapper.find('button');
+    const handleSpy = jest.spyOn(wrapper.instance(), 'toggleFavorite')
+    button.simulate('click');
+    expect(API.deleteFavorite).toHaveBeenCalled()
   })
 
   describe('mapStateToProps', () => {
@@ -41,9 +72,15 @@ describe('Card', () => {
   })
 
   describe('mapDispatchToProps', () => {
+    let favorite
+    let mockDispatch
+
+    beforeEach(() => {
+      favorite = mockMovie
+      mockDispatch = jest.fn()
+    })
+    
     it('should call dispatch with the correct params', () => {
-      const favorite = mockMovie
-      const mockDispatch = jest.fn()
       const actionToDispatch = actions.addFavorite(favorite)
       const result = mapDispatchToProps(mockDispatch)
       result.addFavorite(favorite)
@@ -51,8 +88,6 @@ describe('Card', () => {
     })
 
     it('should call dispatch with the correct params', () => {
-      const favorite = mockMovie
-      const mockDispatch = jest.fn()
       const actionToDispatch = actions.deleteFavoriteFromStore(favorite)
       const result = mapDispatchToProps(mockDispatch)
       result.deleteFavoriteFromStore(favorite)
