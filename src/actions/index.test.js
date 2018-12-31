0519d9948910d2993951de2795becddb303a0c91
Kylie Stanley <knstanley11@gmail.com>
2018-12-30 17:24:55 -0700

    Add action tests

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -21,17 +21,6 @@ describe('actions', () => {
     expect(result).toEqual(expected)
   })
 
-  it('should have a type of FETCH_MOVIES with a movies array', () => {
-    const movies = [{movie: 'great movie'}]
-    const expected = {
-      type: 'FETCH_MOVIES_SUCCESS',
-      movies,
-    }
-
-    const result = actions.fetchMoviesSuccess(movies)
-    expect(result).toEqual(expected)
-  })
-
   it('should have a type of ADD_FAVORITE with a favorite Object', () => {
     const favorite = {movie: 'great movie'}
     const expected = {
@@ -53,7 +42,8 @@ describe('actions', () => {
     const result = actions.deleteFavoriteFromStore(favorite)
     expect(result).toEqual(expected)
   })
-   it('should have a type of ADD_FAVORITES with a favorites array', () => {
+
+  it('should have a type of ADD_FAVORITES with a favorites array', () => {
     const favorites = [{movie: 'great movie'}]
     const expected = {
       type: 'ADD_FAVORITES',
@@ -61,6 +51,50 @@ describe('actions', () => {
     }
 
     const result = actions.addFavorites(favorites)
+    expect(result).toEqual(expected)
+  })
+
+  it('should have a type of IS_LOADING with boolean', () => {
+    const isLoading = true
+    const expected = {
+      type: 'IS_LOADING',
+      isLoading,
+    }
+
+    const result = actions.isLoading(isLoading)
+    expect(result).toEqual(expected)
+  })
+
+  it('should have a type of HAS_ERRORED with an error message ', () => {
+    const message = 'error retrieving info'
+    const expected = {
+      type: 'HAS_ERRORED',
+      message,
+    }
+
+    const result = actions.hasErrored(message)
+    expect(result).toEqual(expected)
+  })
+
+  it('should have a type of FETCH_MOVIES_SUCCESS with a movies array', () => {
+    const movies = [{movie: 'great movie'}]
+    const expected = {
+      type: 'FETCH_MOVIES_SUCCESS',
+      movies,
+    }
+
+    const result = actions.fetchMoviesSuccess(movies)
+    expect(result).toEqual(expected)
+  })
+
+  it('should have a type of SEARCH_MOVIES_SUCCESS with a movies array', () => {
+    const movies = [{movie: 'great movie'}]
+    const expected = {
+      type: 'SEARCH_MOVIES_SUCCESS',
+      movies,
+    }
+
+    const result = actions.searchMovieSuccess(movies)
     expect(result).toEqual(expected)
   })
 })
